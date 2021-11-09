033e2fb734a8e85f15e157170cd2d1d9b797917a
Zemola <azeemolatunji@gmail.com>
2021-11-09 18:54:18 +0100

    Fix lintters error

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// get menu icon element
+// Getting menu icon element
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".desktop-menu");
 
@@ -7,7 +7,7 @@ menu.addEventListener("click", () => {
   menuLinks.classList.toggle("active");
 });
 
-// Get the bars of the hamburger menu
+// Getting the bars of the hamburger menu
 const portfolio = document.querySelector(".portfolio-link");
 const aboutUs = document.querySelector(".about-link");
 const contactLink = document.querySelector(".contact-link");
