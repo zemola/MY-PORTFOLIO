af63578b78e3fd23e8f365ffaed61b3d530a6263
Zemola <azeemolatunji@gmail.com>
2021-11-09 18:06:23 +0100

    Added event listner to the hamburgar menu on app.js file

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-// Menu item fetched with query selector
+// get menu icon element
 const menu = document.querySelector("#mobile-menu");
-const menuLinks = document.querySelector(".nav-menu");
+const menuLinks = document.querySelector(".desktop-menu");
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
 
-// Individual bar for the mobile menu icon
+// Get the bars of the hamburger menu
 const portfolio = document.querySelector(".portfolio-link");
 const aboutUs = document.querySelector(".about-link");
 const contactLink = document.querySelector(".contact-link");
 
-const closeMobileMenu = () => {
+const close = () => {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 };
+
+portfolio.addEventListener("click", () => {
+  close();
+});
+
+aboutUs.addEventListener("click", () => {
+  close();
+});
+
+contactLink.addEventListener("click", () => {
+  close();
+});
