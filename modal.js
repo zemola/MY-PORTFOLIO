d2945cff30b2cffce2f90792bb0824eb1203d2aa
Amkam <amaduskamara@gmail.com>
2021-11-10 19:19:49 +0000

    Add styles for multipost stores modal

--- modal.js
+++ modal.js
@@ -1,39 +1,38 @@
 const projects = [
-    {
-      name: 'Tonic',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis deserunt nesciunt nulla tenetur ipsam dolorum placeat natus aperiam recusandae maiores.',
-      featuredImage: './image/desktopad1.png',
-      technologies: ['html', 'Ruby and rails', 'css', 'javascript'],
-      stacks: ['canopy', 'Backend Dev', '2015'],
-    },
-    {
-      name: 'Multi-Post Stores',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis deserunt nesciunt nulla tenetur ipsam dolorum placeat natus aperiam recusandae maiores.',
-      featuredImage: './image/desktopad2.png',
-      technologies: ['html', 'Ruby and rails', 'css', 'javascript'],
-      stacks: ['canopy', 'Backend Dev', '2015'],
-    },
-    {
-      name: 'Facebook 360',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis deserunt nesciunt nulla tenetur ipsam dolorum placeat natus aperiam recusandae maiores.',
-      featuredImage: './image/desktopad3.png',
-      technologies: ['html', 'Ruby and rails', 'css', 'javascript'],
-      stacks: ['canopy', 'Backend Dev', '2015'],
-    },
-    {
-      name: 'Uber Navigator',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis deserunt nesciunt nulla tenetur ipsam dolorum placeat natus aperiam recusandae maiores.',
-      featuredImage: './image/desktopad4.png',
-      technologies: ['html', 'css', 'javascript', 'Ruby and rails'],
-      stacks: ['canopy', 'Backend Dev', '2015'],
-    },
-  ];
+  {
+    name: 'Tonic',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis deserunt nesciunt nulla tenetur ipsam dolorum placeat natus aperiam recusandae maiores.',
+    featuredImage: './image/desktopad1.png',
+    technologies: ['html', 'Ruby and rails', 'css', 'javascript'],
+    stacks: ['canopy', 'Backend Dev', '2015'],
+  },
+  {
+    name: 'Multi-Post Stores',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis deserunt nesciunt nulla tenetur ipsam dolorum placeat natus aperiam recusandae maiores.',
+    featuredImage: './image/desktopad2.png',
+    technologies: ['html', 'Ruby and rails', 'css', 'javascript'],
+    stacks: ['canopy', 'Backend Dev', '2015'],
+  },
+  {
+    name: 'Facebook 360',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis deserunt nesciunt nulla tenetur ipsam dolorum placeat natus aperiam recusandae maiores.',
+    featuredImage: './image/desktopad3.png',
+    technologies: ['html', 'Ruby and rails', 'css', 'javascript'],
+    stacks: ['canopy', 'Backend Dev', '2015'],
+  },
+  {
+    name: 'Uber Navigator',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis deserunt nesciunt nulla tenetur ipsam dolorum placeat natus aperiam recusandae maiores.',
+    featuredImage: './image/desktopad4.png',
+    technologies: ['html', 'css', 'javascript', 'Ruby and rails'],
+    stacks: ['canopy', 'Backend Dev', '2015'],
+  },
+];
 
-  
 // Tonic Modal Details
 const openTonicCardModal = document.querySelector('#tonic-modal-open');
 const tonicCardModalContainer = document.querySelector('.tonic-card-modal-container');
@@ -49,15 +48,36 @@ closeTonicCardModal.addEventListener('click', () => {
 
 document.querySelector('.tonic-title').textContent = projects[0].name;
 
-document.querySelector('.tonic-stack1').textContent =
-  projects[0].stacks[0].toUpperCase();
+document.querySelector('.tonic-stack1').textContent = projects[0].stacks[0].toUpperCase();
 document.querySelector('.tonic-stack2').textContent = projects[0].stacks[1];
 document.querySelector('.tonic-stack3').textContent = projects[0].stacks[2];
-document.querySelector('.tonic-description').textContent =
-  projects[0].description;
+document.querySelector('.tonic-description').textContent = projects[0].description;
 document.querySelector('.tonic-image').src = projects[0].featuredImage;
 document.querySelector('.tonic-html').textContent = projects[0].technologies[0];
 document.querySelector('.tonic-css').textContent = projects[0].technologies[1];
-document.querySelector('.tonic-javascript').textContent =
-  projects[0].technologies[2];
+document.querySelector('.tonic-javascript').textContent = projects[0].technologies[2];
 document.querySelector('.tonic-ruby').textContent = projects[0].technologies[3];
+
+// Multipost Modal Details
+const openMultipostCardModal = document.querySelector('#multipost-modal-open');
+const multipostCardModalContainer = document.querySelector('.multipost-card-modal-container');
+const closeMultipostCardModal = document.querySelector('#multipost-card-modal-close');
+
+openMultipostCardModal.addEventListener('click', () => {
+  multipostCardModalContainer.classList.add('show');
+});
+
+closeMultipostCardModal.addEventListener('click', () => {
+  multipostCardModalContainer.classList.remove('show');
+});
+document.querySelector('.multipost-title').textContent = projects[1].name;
+
+document.querySelector('.multipost-stack1').textContent = projects[1].stacks[0].toUpperCase();
+document.querySelector('.multipost-stack2').textContent = projects[1].stacks[1];
+document.querySelector('.multipost-stack3').textContent = projects[1].stacks[2];
+document.querySelector('.multipost-description').textContent = projects[1].description;
+document.querySelector('.multipost-image').src = projects[1].featuredImage;
+document.querySelector('.multipost-html').textContent = projects[1].technologies[0];
+document.querySelector('.multipost-css').textContent = projects[1].technologies[1];
+document.querySelector('.multipost-javascript').textContent = projects[1].technologies[2];
+document.querySelector('.multipost-ruby').textContent = projects[1].technologies[3];
